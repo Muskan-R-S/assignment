dae455fef33a0df50f8d01330b8e6b7ca8531e32
Muskan Shaikh <76590770+Muskan-R-S@users.noreply.github.com>
2024-11-26 13:18:53 -0800

    Added Tests and Improved Code

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,31 @@
 from flask import Flask
 import time
 from flask import jsonify
+import os
 
 app = Flask(__name__)
 
 
 
-SECONDS_DELAY = 200
-start_time = time.time()
-
+app.config["SECONDS_DELAY"] = int(os.getenv("SECONDS_DELAY", 50))
+app.config["START_TIME"] = time.time()
 
 @app.route("/status")
 def get_status():
-    cur_time = time.time()
-    diff_in_seconds = cur_time - start_time
-    try:
-        if  diff_in_seconds > SECONDS_DELAY:
+    try: 
+        cur_time = time.time()
+        diff_in_seconds = cur_time - app.config["START_TIME"] 
+        
+        if  diff_in_seconds > app.config["SECONDS_DELAY"] :
             print("Completed")
             result = "Completed"
         else:
             print("Pending")
             result = "Pending"
-    except:
+    except Exception:
+        print(f"Error:{Exception}")
         result = "error"
+        return jsonify({"result": result}), 404
 
-    return jsonify({"result": result})
+    return jsonify({"result": result}), 200
     
